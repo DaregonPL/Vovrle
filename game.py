94a8194cc41e392656f2b5ae9f525d12dc5212af
daregonplay@gmail.com <126910533+DaregonPL@users.noreply.github.com>
2025-03-09 17:56:30 +0300

    Main screen Main screen added

--- game.py
+++ game.py
@@ -7,4 +7,29 @@ Vovrle - Wordle, but worse
 
 class Game:
     def __init__(self):
-        pass
+        self.exit = False
+        while not self.exit:
+            self.menu()
+
+    def menu(self):
+        ChoiceMM = Choice([
+            'Play', 'Settings', 'Save & Exit'
+        ], 'Welcome to Vovrle', ['help', 'about'])
+        ChoiceMM.display()
+        ans = ChoiceMM.answer()
+        if ans == 'Play':
+            pass
+        elif ans == 'Settings':
+            pass
+        elif ans == 'Save & Exit':
+            pass
+        elif ans == 'help':
+            pass
+        elif ans == 'about':
+            pass
+        else:
+            print('fuvk')
+
+
+if __name__ == '__main__':
+    Game()
